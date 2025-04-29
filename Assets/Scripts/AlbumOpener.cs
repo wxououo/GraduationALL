eaad750f010f3b795d8987facafdfcf28ba9179d
wxououo <S111019010@stu.ntue.edu.tw>
2025-04-29 09:19:31 +0800

    Revert "4/29F"
    This reverts commit 8f08f03c072ecf0193a114cb00768f49ae26f926.

--- Assets/Scripts/AlbumOpener.cs
+++ Assets/Scripts/AlbumOpener.cs
@@ -2,18 +2,31 @@ using UnityEngine;
 
 public class AlbumOpener : MonoBehaviour
 {
-    public GameObject SceneCamera;
-    [SerializeField] private GameObject buttonToHide;
-    public GameObject AlbumFrame;
+    public CanvasGroup albumCanvasGroup;
+    public GameObject buttonToHide;
+    private bool isOpen = false;
+    void Start()
+    {
+        // 確保 albumCanvasGroup 不為空
+        if (albumCanvasGroup != null)
+        {
+            // 將 alpha 設定為 0，使其完全透明
+            albumCanvasGroup.alpha = 0f;
+        }
+    }
+
     public void OpenAlbum()
     {
-        if (AlbumFrame != null)
+        if (albumCanvasGroup != null)
         {
-            bool isActive = AlbumFrame.activeSelf;
-            AlbumFrame.SetActive(!isActive);
-            if (buttonToHide != null && !SceneCamera.activeInHierarchy)
+            isOpen = !isOpen;
+            albumCanvasGroup.alpha = isOpen ? 0f : 1f;
+            albumCanvasGroup.interactable = !isOpen;
+            albumCanvasGroup.blocksRaycasts = !isOpen;
+
+            if (buttonToHide != null)
             {
-                buttonToHide.SetActive(isActive);
+                buttonToHide.SetActive(isOpen);
             }
         }
     }
